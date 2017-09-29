cbc1aeebd354865e41836193d752ff510db9c23c
michaeljpitz <michaeljpitz@gmail.com>
2017-09-29 11:57:13 +0200

    variable get

--- src/AppBundle/Controller/AccueilController.php
+++ src/AppBundle/Controller/AccueilController.php
@@ -32,4 +32,13 @@ class AccueilController extends Controller
         // replace this example code with whatever you need
         return $this->render('perso/pg3.html.twig');
     }
+    /**
+     * lorsque on clique sur hello/ on passe une variable qui a comme valeur par défaut: anonyme
+     * @Route("/hello/{qui}", name="bonjour", defaults={"qui" = "anonyme"})
+     */
+    public function helloAction($qui)
+    {
+        // replace this example code with whatever you need
+        return $this->render('perso/hello.html.twig',["nom"=>$qui]);
+    }
 }
